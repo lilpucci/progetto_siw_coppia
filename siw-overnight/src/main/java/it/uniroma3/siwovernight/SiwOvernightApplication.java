30bcd01886fcefa5b5354ac4f9c8b418c3772218
andrea <andrealippo01@gmail.com>
2024-05-06 15:26:45 +0200

    prova commit

---
+++ siw-overnight/src/main/java/it/uniroma3/siwovernight/SiwOvernightApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siwovernight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwOvernightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwOvernightApplication.class, args);
+	}
+
+}
---
+++ siw-overnight/src/test/java/it/uniroma3/siwovernight/SiwOvernightApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siwovernight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwOvernightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
